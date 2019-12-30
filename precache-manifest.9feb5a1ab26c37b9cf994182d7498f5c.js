c802d6b4dd23c2d31d4e5244a78a293596aa29bb
rizwanishaq <rizwanishaq@gmail.com>
2019-12-30 12:55:54 +0100

    Updates

---
+++ precache-manifest.9feb5a1ab26c37b9cf994182d7498f5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fafa3425ad8113afb3ca28c6c0116b5",
+    "url": "/coin/index.html"
+  },
+  {
+    "revision": "fb7bb857fe419d169ebb",
+    "url": "/coin/static/css/main.afade7f3.chunk.css"
+  },
+  {
+    "revision": "601dc7c2a59ed8c95b7a",
+    "url": "/coin/static/js/2.17e6469a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/coin/static/js/2.17e6469a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb7bb857fe419d169ebb",
+    "url": "/coin/static/js/main.04cb35ac.chunk.js"
+  },
+  {
+    "revision": "429ba99eee9aa17a571f",
+    "url": "/coin/static/js/runtime-main.8f252d54.js"
+  }
+]);
